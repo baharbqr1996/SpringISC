6b6e275267b7e0712a51f7018c871a2e09062845
bahareh <57172109+baharbqr1996@users.noreply.github.com>
2024-09-09 03:49:28 -0700

    Delete session5/repo/categoryRepoRun.java

--- session5/repo/categoryRepoRun.java
+++
@@ -1,9 +0,0 @@
-package ir.freeland.springboot.persistence.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ir.freeland.springboot.persistence.model.Category;
-
-public interface categoryRepoRun extends CrudRepository<Category, Long>{
-
-}
